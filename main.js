f16bd5be941896e083d0d43c05b0f11b962452cd
CryptoCat420 <87661267+CryptoCat420@users.noreply.github.com>
2021-12-21 03:30:11 +0100

    Init

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width : 1200,
+        height: 600,
+        enableRemoteModule: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "/public/index.html"));
+}
+
+app.on("ready", loadMainWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+      loadMainWindow();
+  }
+});
+
+
+/* Hot reload */
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
