ae7b22d73cf6d53819bd49c09a2e2221db76cc50
DevvCat <87661267+CryptoCat420@users.noreply.github.com>
2022-03-19 19:43:25 +0100

    [config.js] Added sfp connection

--- backend-server/config.js
+++ backend-server/config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  
   daemonHostname: "127.0.0.1",
   daemonPort: 22101,
+
+  sfpProtocol: "http",
+  sfpHostname: "127.0.0.1",
+  sfpPort: 22104,
+  sfpVersion: "v1",
+
   walletFilename: "wallet.dat",
   decimals: 8,
   ticker: "SNW"
