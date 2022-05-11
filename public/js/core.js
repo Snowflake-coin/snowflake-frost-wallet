6434bcfdbb33c39a847d0b2f2ae80c77e21e7d16
CryptoCat420 <87661267+CryptoCat420@users.noreply.github.com>
2022-05-11 14:02:16 +0200

    [js] Created core.js

---
+++ public/js/core.js
@@ -0,0 +1,25 @@
+/* Convert timestamp to month and day */
+function timestampDate(timestamp) {
+	let date = new Date(timestamp * 1000);
+	return {
+		month: date.toLocaleString('default', { month: 'long' }).substring(0, 3),
+		day: date.getDate()
+	};
+}
+
+/* Copy wallet address */
+let copyInputActive = false;
+function copyInput(dom) {
+  dom.select();
+
+  navigator.clipboard.writeText(dom.value);
+
+  document.getElementById('copiedMessage').style.opacity = "1";
+  if(copyInputActive == false) {
+    copyInputActive = true;
+    setTimeout(() => {
+      document.getElementById('copiedMessage').style.opacity = "0";
+      copyInputActive = false;
+    }, 3000);
+  }
+}
