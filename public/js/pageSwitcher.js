cc850f658c6d17e474833673f8aa15eb4487147c
CryptoCat420 <87661267+CryptoCat420@users.noreply.github.com>
2022-05-11 14:03:34 +0200

    [pageSwitcher.js] Advancify page switcher

--- public/js/pageSwitcher.js
+++ public/js/pageSwitcher.js
@@ -1,10 +1,11 @@
-let currentPage = 'dashboard';
-function switchPage(dom) {
+function switchPage(dom, otherPage, forceClick) {
   /* Dom elements */
   const dashboardPage = document.getElementById('dashboardPage');
   const sendPage = document.getElementById('sendPage');
   const receivePage = document.getElementById('receivePage');
   const tokensPage = document.getElementById('tokensPage');
+  const createTokenPage = document.getElementById('createTokenPage');
+  const manageTokensPage = document.getElementById('manageTokensPage');
   const settingsPage = document.getElementById('settingsPage');
 
   /* Hide all pages */
@@ -13,10 +14,22 @@ function switchPage(dom) {
     sendPage.classList.add('d-none');
     receivePage.classList.add('d-none');
     tokensPage.classList.add('d-none');
+    createTokenPage.classList.add('d-none');
+    manageTokensPage.classList.add('d-none');
     settingsPage.classList.add('d-none');
   }
 
-  if(dom.id == 'aDashboard' && dom.classList.value !== 'hover') {
+  if(otherPage == 'createTokenPage') {
+    if(sfpConnection) {
+      hideAll();
+      createTokenPage.classList.remove('d-none');
+    } else {
+      notify('Can\'t create token. No connection to SFP node', 'error', 3)
+    }
+  } else if(otherPage == 'manageTokensPage') {
+    hideAll();
+    manageTokensPage.classList.remove('d-none');
+  } else if(dom.id == 'aDashboard' && dom.classList.value !== 'hover') {
     hideAll();
     dashboardPage.classList.remove('d-none');
   } else if(dom.id == 'aSend' && dom.classList.value !== 'hover') {
@@ -25,9 +38,15 @@ function switchPage(dom) {
   } else if(dom.id == 'aReceive' && dom.classList.value !== 'hover') {
     hideAll();
     receivePage.classList.remove('d-none');
-  } else if(dom.id == 'aTokens' && dom.classList.value !== 'hover') {
-    hideAll();
-    tokensPage.classList.remove('d-none');
+  } else if(dom.id == 'aTokens') {
+    if(forceClick == true) {
+      hideAll();
+      tokensPage.classList.remove('d-none');
+    } else if(dom.classList.value !== 'hover') {
+      hideAll();
+      tokensPage.classList.remove('d-none');
+    }
+    
   } else if(dom.id == 'aSettings' && dom.classList.value !== 'hover') {
     hideAll();
     settingsPage.classList.remove('d-none');
@@ -42,4 +61,5 @@ function switchPage(dom) {
   dom.classList = 'hover';
 }
 
-switchPage(document.getElementById('aTokens'));
+switchPage(document.getElementById('aDashboard'));
+//switchPage(document.getElementById('aTokens'), 'manageTokensPage')
