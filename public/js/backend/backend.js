8bb0d8b37377530f0be7f5485d9a1b93670e5c89
CryptoCat420 <87661267+CryptoCat420@users.noreply.github.com>
2022-05-20 02:04:07 +0200

    [backend.js] Daemon and node addresses in settings

--- public/js/backend/backend.js
+++ public/js/backend/backend.js
@@ -119,6 +119,10 @@ async function main() {
 	if(settings_db.getData('/dark_mode')) { 
 		document.getElementById('darkModeSwitcher').checked = true;
 		document.getElementById('darkModeCSS').href = `css/style_dark.css`;
+	}
+	document.getElementById('sfpNodeAddress').value = settings_db.getData('/sfp_address');
+	document.getElementById('daemonAddress').value = settings_db.getData('/daemon_address');
+
 	try {
 		let coinPaprikaReq = await fetch(`https://api.coinpaprika.com/v1/coins/snw-snowflake-network/ohlcv/today?quote=btc`, {
 			"method": "GET",
